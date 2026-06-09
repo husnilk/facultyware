d4e300ac21f0cc141b8a6068ea780a4dd2390d49
zizisalsabila573-jpg <zizisalsabila573@gmail.com>
2026-06-09 16:36:44 +0700

    update dashboard pegawai

--- controllers/pegawaiController.js
+++ controllers/pegawaiController.js
@@ -1,3 +1,11 @@
 exports.index = (req, res) => {
-    res.send('Ini adalah halaman Dashboard Pegawai (Tugas Zizi)');
+    res.send(`
+        <h1>Dashboard Pegawai</h1>
+
+        <ul>
+            <li><a href="/pegawai/cuti">Riwayat Pengajuan Cuti</a></li>
+            <li><a href="/pegawai/cuti/tambah">Buat Pengajuan Cuti</a></li>
+            <li><a href="/pegawai/notifikasi">Notifikasi</a></li>
+        </ul>
+    `);
 };
--- routes/pegawai.js
+++ routes/pegawai.js
@@ -4,10 +4,20 @@ const pegawaiController = require('../controllers/pegawaiController');
 const { isAuthenticated } = require('../middlewares/auth');
 const acl = require('../middlewares/acl');
 
-// Kunci rute ini khusus untuk Pegawai
 router.use(isAuthenticated, acl.checkRole('pegawai'));
 
-// Endpoint utama
 router.get('/', pegawaiController.index);
+
+router.get('/cuti', (req, res) => {
+    res.send('Halaman Riwayat Pengajuan Cuti');
+});
+
+router.get('/cuti/tambah', (req, res) => {
+    res.send('Form Pengajuan Cuti');
+});
+
+router.get('/notifikasi', (req, res) => {
+    res.send('Halaman Notifikasi Pegawai');
+});
 
 module.exports = router;
