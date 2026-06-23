a07555d8136d46dc96ccf9bb89f7f30a14e74631
akrammakruf <akrammakrufaidil@gmail.com>
2026-06-23 07:58:08 +0700

    fix: register ghufran routes in app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,17 +9,20 @@ var MySQLStore = require('express-mysql-session')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const { notFoundHandler, errorHandler } = require('./middlewares/error');
-
-var app = express();
+var adminRouter = require('./routes/admin');
 
 var remindersRouter = require('./routes/reminders');
 var checkinsRouter = require('./routes/checkins');
 var attendancesRouter = require('./routes/attendances');
 var apiAkramRouter = require('./routes/apiAkram');
+
 var apiGhufranRouter = require('./routes/apiGhufran');
 var registrationsRouter = require('./routes/registrations');
 var ticketsRouter = require('./routes/tickets');
+
+const { notFoundHandler, errorHandler } = require('./middlewares/error');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,8 +33,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-var adminRouter = require('./routes/admin');
-app.use('/admin', adminRouter);
 
 // Session configuration
 const sessionStore = new MySQLStore({
@@ -40,26 +42,37 @@ const sessionStore = new MySQLStore({
   database: process.env.DB_NAME,
 });
 
-app.use(session({
-  key: 'session_cookie_name',
-  secret: process.env.SESSION_SECRET || 'secret',
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 // 1 day
-  }
-}));
+app.use(
+  session({
+    key: 'session_cookie_name',
+    secret: process.env.SESSION_SECRET || 'secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
+// Main routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Akram routes
 app.use('/reminders', remindersRouter);
 app.use('/checkins', checkinsRouter);
 app.use('/attendances', attendancesRouter);
 app.use('/api/akram', apiAkramRouter);
-app.use('/api/ghufran', apiGhufranRouter);
+
+// Ghufran routes
 app.use('/events', registrationsRouter);
+app.use('/registrations', registrationsRouter);
 app.use('/tickets', ticketsRouter);
+app.use('/api/ghufran', apiGhufranRouter);
+
+// Admin routes
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(notFoundHandler);
